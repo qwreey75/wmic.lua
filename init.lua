7585506207822b3973f79c50113b8d39317b8efd
세젤귀 고양이들 <46598063+qwreey75@users.noreply.github.com>
2022-03-19 18:30:35 +0900

    Added more options

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@
 local spawn = require"coro-spawn";
 
 local module = {};
+local async = promise.async;
 local proc = spawn("wmic",{args = {},stdio = {true,true,true}});
 local stdinWrite = proc.stdin.write;
 local insert = table.insert;
@@ -13,19 +14,19 @@ local events = {};
 module.proc = proc;
 
 local pack = table.pack;
-local function unpack(t,n,p)
-	if not n then n = t.n or #t end
-	if (not n) or n == 0 then
-		return;
-	elseif not p then
-		if n == 1 then return t[1]; end
-		p = 1;
-		return t[1],unpack(t,n,p+1);
-	elseif n >= p then
-		return t[n];
-	end
-	return t[p],unpack(t,n,p+1);
-end
+-- local function unpack(t,n,p)
+-- 	if not n then n = t.n or #t end
+-- 	if (not n) or n == 0 then
+-- 		return;
+-- 	elseif not p then
+-- 		if n == 1 then return t[1]; end
+-- 		p = 1;
+-- 		return t[1],unpack(t,n,p+1);
+-- 	elseif n >= p then
+-- 		return t[n];
+-- 	end
+-- 	return t[p],unpack(t,n,p+1);
+-- end
 
 wrap(function()
 	for str in proc.stdout.read do
@@ -38,10 +39,11 @@ wrap(function()
                 elseif patternType == "function" then
 					args = pack(pattern(str));
 				end
-				if args.n ~= 0 then
+				local first = args[1];
+				if first and first ~= "" then
 					events[pattern] = {};
-                    for _,func in ipairs(waitters) do
-                        resume(func,unpack(args));
+                    for _,waitting in ipairs(waitters) do
+                        resume(waitting,unpack(args));
                     end
 				end
                 break;
@@ -59,11 +61,28 @@ function module.addEventListener(event,cmd)
 end
 local addEventListener = module.addEventListener;
 
-local getCpuLoadpercentage = "LoadPercentage\n-(%d+)";
-local getCPuLoadpercentageCommand = "cpu get loadpercentage\n";
+local getCpuLoadpercentage = "LoadPercentage[ \t\n\r]*(%d+)";
+local getCpuLoadpercentageCommand = "cpu get loadpercentage\n";
 events[getCpuLoadpercentage] = {};
 function module.getCpuLoadpercentage()
-	return addEventListener(getCpuLoadpercentage,getCPuLoadpercentageCommand);
+	return tonumber(addEventListener(getCpuLoadpercentage,getCpuLoadpercentageCommand));
 end
+module.getCpuLoadpercentageAsync = async(module.getCpuLoadpercentage);
+
+local getFreePhysicalMemory = "FreePhysicalMemory[ \t\n\r]*(%d+)";
+local getFreePhysicalMemoryCommand = "OS GET FreePhysicalMemory\n";
+events[getFreePhysicalMemory] = {};
+function module.getFreePhysicalMemory()
+	return tonumber(addEventListener(getFreePhysicalMemory,getFreePhysicalMemoryCommand));
+end
+module.getFreePhysicalMemoryAsync = async(module.getFreePhysicalMemory);
+
+local getTotalPhysicalMemory = "TotalPhysicalMemory[ \t\n\r]*(%d+)";
+local getTotalPhysicalMemoryCommand = "ComputerSystem GET TotalPhysicalMemory\n";
+events[getTotalPhysicalMemory] = {};
+function module.getTotalPhysicalMemory()
+	return tonumber(addEventListener(getTotalPhysicalMemory,getTotalPhysicalMemoryCommand));
+end
+module.getTotalPhysicalMemoryAsync = async(module.getTotalPhysicalMemory);
 
 return module;
